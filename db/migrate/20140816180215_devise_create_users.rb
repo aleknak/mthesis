f25cc2d6d690c48aba4fdc9c7901aa92877a71e2
Aleksandar Nakinov <an@medialab.io>
2014-10-22 00:16:24 +0200

    Added name and is_admin to users

--- db/migrate/20140816180215_devise_create_users.rb
+++ db/migrate/20140816180215_devise_create_users.rb
@@ -1,6 +1,7 @@
 class DeviseCreateUsers < ActiveRecord::Migration
   def change
     create_table(:users) do |t|
+      t.string :name,               null: false, default: ""
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
@@ -31,6 +32,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
       # t.datetime :locked_at
 
 
+      t.boolean :is_admin, default: false
       t.timestamps
     end
 
